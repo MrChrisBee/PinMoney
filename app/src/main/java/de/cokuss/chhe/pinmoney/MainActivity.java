a0bb62edbb2b48190c54afa7f5da1c77bb6769ac
hemaassen <helgemaassen@yaoo.de>
2016-09-14 12:10:22 +0200

    14.09.16 -12:10Uhr

--- app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
+++ app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
@@ -2,12 +2,20 @@ package de.cokuss.chhe.pinmoney;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.empfaenger_neu);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
     }
 }
