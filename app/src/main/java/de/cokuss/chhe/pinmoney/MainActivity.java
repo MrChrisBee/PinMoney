8a5474477032fdd2a4ae4abf661d36f5ab481848
hemaassen <helgemaassen@yaoo.de>
2016-09-14 13:31:41 +0200

    14.09.16 -13:30Uhr

--- app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
+++ app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_eltern);
+        setContentView(R.layout.first_start);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
