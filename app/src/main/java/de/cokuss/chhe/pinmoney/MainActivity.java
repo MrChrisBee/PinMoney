2da319bafee02b46635b3fa3e9416c14c73756b5
MrChrisBee <chris@cokuss.de>
2016-09-15 15:01:13 +0200

    layout Test

--- app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
+++ app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
@@ -10,21 +10,43 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.first_start);
+    String viewInfo;
 
-        Button b = (Button)findViewById(R.id.button_parents);
-        b.setOnClickListener(new View.OnClickListener(){
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // welche Version der App nutzen wir
+        if (savedInstanceState == null) {
+            // nur beim ersten Start first_start anzeigen
+            savedInstanceState.putString("inView", "first_start");
+            setContentView(R.layout.first_start);
+        }
+
+        // dann nach
+        if ((viewInfo = (String) savedInstanceState.get("inView")) != null) {
+            switch (viewInfo) {
+                case "main_eltern":
+                    setContentView(R.layout.main_eltern);
+                    break;
+            }
+
+        } else {
+
+        }
+
+        Button b = (Button) findViewById(R.id.button_parents);
+        b.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
+                // über Bundle festhalten wo wir sind
+                savedInstanceState.putString("inView", "main_eltern");
                 setContentView(R.layout.main_eltern);
             }
         });
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
