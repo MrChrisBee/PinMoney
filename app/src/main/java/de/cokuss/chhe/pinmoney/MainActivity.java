2ae868dcdeab13d6fe56fd6632f3c4a961f79dcf
MrChrisBee <chris@cokuss.de>
2016-09-15 15:17:50 +0200

    layout Test

--- app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
+++ app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
@@ -10,28 +10,30 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    String viewInfo;
+    String viewInfo = "first_start";
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("inView", viewInfo);
+    }
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // welche Version der App nutzen wir
-        if (savedInstanceState == null) {
-            // nur beim ersten Start first_start anzeigen
-            savedInstanceState.putString("inView", "first_start");
-            setContentView(R.layout.first_start);
-        }
-
-        // dann nach
         if ((viewInfo = (String) savedInstanceState.get("inView")) != null) {
             switch (viewInfo) {
                 case "main_eltern":
                     setContentView(R.layout.main_eltern);
                     break;
+                case "first_start":
+                    setContentView(R.layout.first_start);
+                    break;
             }
 
         } else {
-
+            setContentView(R.layout.first_start);
         }
 
         Button b = (Button) findViewById(R.id.button_parents);
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // über Bundle festhalten wo wir sind
-                savedInstanceState.putString("inView", "main_eltern");
+                viewInfo = "main_eltern";
                 setContentView(R.layout.main_eltern);
             }
         });
