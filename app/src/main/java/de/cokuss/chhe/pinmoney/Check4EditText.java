d501ec2f68c96906ca3f4855589b79ea377b947a
Christian Beerwerth <icke@cokuss.de>
2016-10-13 21:44:18 +0200

    noch mit abbruch nach Neuanlage

---
+++ app/src/main/java/de/cokuss/chhe/pinmoney/Check4EditText.java
@@ -0,0 +1,26 @@
+package de.cokuss.chhe.pinmoney;
+
+class Check4EditText {
+    private String string;
+    private boolean valid;
+    private EditText editText;
+
+    Check4EditText(EditText editText, String string, boolean aBoolean) {
+        this.editText = editText;
+        this.string = string;
+        this.valid = aBoolean;
+    }
+
+    //Check4Edit enthält deswegen ein EditText um in string2Date möglicherweise darauf einen Fehlertext zu legen
+    EditText getEditText() {
+        return editText;
+    }
+
+    boolean isValid() {
+        return valid;
+    }
+
+    public String getString() {
+        return string;
+    }
+}
