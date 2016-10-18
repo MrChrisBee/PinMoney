11632d8f5f6689a90c3607d806e1f0f90e4f626d
hemaassen <helgemaassen@yahoo.de>
2016-10-18 13:24:13 +0200

    18.10.2016 - 13:20

---
+++ app/src/main/java/de/cokuss/chhe/pinmoney/HelpBookingActivity.java
@@ -0,0 +1,13 @@
+package de.cokuss.chhe.pinmoney;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HelpBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_booking);
+    }
+}
