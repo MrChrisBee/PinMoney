937bb96b7040d7d7da8ec669b25a24ad289b91c9
hemaassen <helgemaassen@yahoo.de>
2016-10-25 10:55:52 +0200

    25.10.2016 - 10:55

--- app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
+++ app/src/main/java/de/cokuss/chhe/pinmoney/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -50,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setIcon(R.mipmap.ic_launcher_money);
+        actionBar.setIcon(R.mipmap.ic_launcher);
     }
 
     private void init() {
